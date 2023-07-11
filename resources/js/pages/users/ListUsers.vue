8e7b657a528a59ffbae357f2387c01c8fa753293
Nanda <nandacoje22@gmail.com>
2023-07-11 22:05:27 +0700

    Mengambil data user dari table users | Laravel vue project

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,16 +1,65 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, ref } from "vue";
+
+const users = ref([]);
+
+const getUsers = () => {
+  axios.get("/api/users").then((response) => {
+    users.value  = response.data;
+  });
+};
+
+onMounted(() => {
+  getUsers();
+});
+</script>
+
 <template>
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">List User</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item active">Users</li>
-            </ol>
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">List User</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item active">Users</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="card">
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Registered</th>
+                  <th>Role</th>
+                  <th>Options</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(user, index) in users" :key="user.id">
+                  <td>{{ index + 1 }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.email }}</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
